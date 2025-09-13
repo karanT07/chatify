208560fba57ef002a937f5c5733d06370e5b84d2
Karan <realkaran07@gmail.com>
2025-09-14 03:06:31 +0530

    Fix formatting in generateToken function in utils.js

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -2,6 +2,7 @@ import jwt from "jsonwebtoken";
 // import { ENV } from "./env.js";
 
 export const generateToken = (userId, res) => {
+    
     const { JWT_SECRET } = process.env;
     if (!JWT_SECRET) {
         throw new Error("JWT_SECRET is not configured");
