0df6ae74eae08b757407194ac968edaa787df845
Karan <realkaran07@gmail.com>
2025-09-14 03:00:28 +0530

    signup section code refactoring

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -34,13 +34,13 @@ export const signup = async (req, res) => {
 
         if (newUser) {
             // before CR:
-            generateToken(newUser._id, res);
-            await newUser.save();
+            // generateToken(newUser._id, res);
+            // await newUser.save();
 
             // after CR:
             // Persist user first, then issue auth cookie
-            // const savedUser = await newUser.save();
-            // generateToken(savedUser._id, res);
+            const savedUser = await newUser.save();
+            generateToken(savedUser._id, res);
 
             res.status(201).json({
                 _id: newUser._id,
